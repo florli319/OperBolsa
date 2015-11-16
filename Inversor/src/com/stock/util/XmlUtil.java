122e52d0f77fb1abb2f906b2db82f1e7fd4f04cf
mjleongithub <mjleon@gmail.com>
2015-11-16 05:53:42 -0500

    Update XmlUtil.java

--- Inversor/src/com/stock/util/XmlUtil.java
+++ Inversor/src/com/stock/util/XmlUtil.java
@@ -1,45 +1,30 @@
 package com.stock.util;
-
-import java.io.File;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
-import com.stock.dto.ActionDTO;
 
 public class XmlUtil {
-	
+
 	/**
-	 * Método que transforma un objeto a un xml
+	 * MÃ©todo que transforma un objeto a un xml
 	 */
-	public static void marshallObtjectToXml(String pathFile){
-
-			  ActionDTO actionDTO = new ActionDTO();
-			  actionDTO.setEmpresa(" AAA " );
-			  actionDTO.setPrecioCompra(100.0);
-
-			  try {
-
-				File file = new File(pathFile);
-				JAXBContext jaxbContext = JAXBContext.newInstance(ActionDTO.class);
-				Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
-				// output pretty printed
-				jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-				jaxbMarshaller.marshal(actionDTO, file);
-				jaxbMarshaller.marshal(actionDTO, System.out);
-
-			      } catch (JAXBException e) {
-				e.printStackTrace();
-			      }
-
+	public static void marshallObtjectToXml(Object jaxbObject, String pathFile){
+	  try {
+		File file = new File(pathFile);
+		JAXBContext jaxbContext = JAXBContext.newInstance(jaxbObject.getClass());
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+   		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		jaxbMarshaller.marshal(jaxbObject, file);
+		jaxbMarshaller.marshal(jaxbObject, System.out);
+		} catch (JAXBException e) {
+		  e.printStackTrace();
+		}
 	}
 	
 	/**
-	 * Método que transforma un xml a un objeto
+	 * MÃ©todo que transforma un xml a un objeto
 	 */
 	public static Object unmarshallXmlToObject(Class clase, String pathFile){
 		 try {
@@ -56,14 +41,6 @@ public class XmlUtil {
 				e.printStackTrace();
 			  }
 		 return null;
-	}
+	}	
 	
-	public void test(){
-		ActionDTO actionDTO = (ActionDTO) unmarshallXmlToObject(ActionDTO.class, "D:\\especializacion\\Doc xml patrones\\file.xml");
-		System.out.println("sirvio " + actionDTO.getEmpresa());
-	}
-	public static void main(String[] args) {
-		new XmlUtil().test();
-	}
-
 }
