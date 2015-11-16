48d36c575d2a300bef12915aef76c04dc5087719
mjleongithub <mjleon@gmail.com>
2015-11-16 06:11:19 -0500

    Create Accion.java

---
+++ Inversor/src/com/stock/inversor/Accion.java
@@ -0,0 +1,20 @@
+package com.stock.inversor;
+
+public class Accion {
+  private String empresa;
+  private int cantidad;
+
+  public String getEmpresa() {
+	return empresa;
+  }
+  public void setEmpresa(String empresa) {
+	this.empresa = empresa;
+  }
+  public int getCantidad() {
+	return cantidad;
+  }
+  public void setCantidad(int cantidad) {
+	this.cantidad = cantidad;
+  }
+  
+}
