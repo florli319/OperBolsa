14b768cbbf11b7c0795217f1052a985046adcfe2
mjleongithub <mjleon@gmail.com>
2015-11-19 16:57:53 -0500

    Create MessageQueueEndPoint.java

---
+++ Inversor/src/com/stock/broker/MessageQueueEndPoint.java
@@ -0,0 +1,47 @@
+package com.stock.broker;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public abstract  class MessageQueueEndPoint {
+	protected Connection connection;
+	protected Channel channel;
+	protected String endPointName;
+
+	public MessageQueueEndPoint(String endpointName) throws IOException, TimeoutException {
+		this.endPointName = endpointName;
+
+		// Create a connection factory
+		ConnectionFactory factory = new ConnectionFactory();
+
+		// hostname of your rabbitmq server
+		factory.setHost("localhost");
+
+		// getting a connection
+		connection = factory.newConnection();
+
+		// creating a channel
+		channel = connection.createChannel();
+
+		// declaring a queue for this channel. If queue does not exist,
+		// it will be created on the server.
+		channel.queueDeclare(endpointName, false, false, false, null);
+	}
+
+	/**
+	 * Close channel and connection. Not necessary as it happens implicitly any
+	 * way.
+	 * 
+	 * @throws IOException
+	 * @throws TimeoutException 
+	 */
+	public void close() throws IOException, TimeoutException {
+		this.channel.close();
+		this.connection.close();
+	}
+
+}
