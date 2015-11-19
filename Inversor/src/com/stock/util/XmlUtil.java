e8c412f76df4bd5890ec09f8bee7a56ed13c3fa4
mjleongithub <mjleon@gmail.com>
2015-11-19 16:52:28 -0500

    Update XmlUtil.java

--- Inversor/src/com/stock/util/XmlUtil.java
+++ Inversor/src/com/stock/util/XmlUtil.java
@@ -1,47 +1,43 @@
 package com.stock.util;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+
 import java.io.File;
 
-
 public class XmlUtil {
-
 	/**
-	 * Método que transforma un objeto a un xml
+	 * Metodo que transforma un objeto a un xml
 	 */
-	public static void marshallObtjectToXml(Object jaxbObject, String pathFile){
-	  try {
-		File file = new File(pathFile);
-		JAXBContext jaxbContext = JAXBContext.newInstance(jaxbObject.getClass());
-		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-   		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		jaxbMarshaller.marshal(jaxbObject, file);
-		jaxbMarshaller.marshal(jaxbObject, System.out);
+	public static void marshallObtjectToXml(Object jaxbObject, String pathFile) {
+		try {
+			File file = new File(pathFile);
+			JAXBContext jaxbContext = JAXBContext.newInstance(jaxbObject
+					.getClass());
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			jaxbMarshaller.marshal(jaxbObject, file);
+			jaxbMarshaller.marshal(jaxbObject, System.out);
 		} catch (JAXBException e) {
-		  e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Método que transforma un xml a un objeto
+	 * Metodo que transforma un xml a un objeto
 	 */
-	public static Object unmarshallXmlToObject(Class clase, String pathFile){
-		 try {
+	@SuppressWarnings("unchecked")
+	public static <T> T unmarshallXmlToObject(Class<T> clase, String pathFile) {
+		try {
+			JAXBContext jc = JAXBContext.newInstance(clase);
+			Unmarshaller u = jc.createUnmarshaller();
+			return (T) u.unmarshal(new File(pathFile));
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-				File file = new File(pathFile);
-				JAXBContext jaxbContext = JAXBContext.newInstance(clase);
-
-				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-				Object obj = (Object) jaxbUnmarshaller.unmarshal(file);
-				System.out.println(obj);
-
-				return obj;
-			  } catch (JAXBException e) {
-				e.printStackTrace();
-			  }
-		 return null;
-	}	
-	
 }
